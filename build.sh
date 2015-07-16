e8e6110da0f66354c5d5dbd500791dab9071c1a1
James Strachan <james.strachan@gmail.com>
2015-07-16 14:40:09 +0100

    added first spike

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+
+DEST="default"
+mkdir -p $DEST
+
+FABRIC8_VERSION=${FABRIC8_VERSION:-2.2.9.1}
+
+echo "Installing templates from fabric8 version #{FABRIC8_VERSION}"
+for app in base management logging metrics chat cdelivery-core cdelivery apiman messaging ipaas kitchen-sink quickstarts; do
+  echo "Installing ${app} template"
+  MVN_URL="http://central.maven.org/maven2/io/fabric8/apps/${app}/${FABRIC8_VERSION}/${app}-${FABRIC8_VERSION}-kubernetes.json"
+  curl -L -H "Accept: */*" ${MVN_URL} -o $DEST/${app}.json
+done
+
+echo "Installing quickstart templates from fabric8 version #{FABRIC8_VERSION}"
+for app in chaos-monkey fabric8mq-producer fabric8mq-consumer prometheus grafana2; do
+  echo "Installing ${app} app template"
+  MVN_QUICKSTART_URL="http://central.maven.org/maven2/io/fabric8/jube/images/fabric8/${app}/${FABRIC8_VERSION}/${app}-${FABRIC8_VERSION}-kubernetes.json"
+  curl -L -H "Accept: */*" ${MVN_QUICKSTART_URL} -o $DEST/${app}.json
+done
+
+echo "Installed templates"
+ls -l $DEST
+
