1d1e1c30d584aa651a1c9331142757a562fe5f10
James Rawlings <jamesrawlings@rely.eclipse.co.uk>
2015-10-26 17:37:28 +0000

    fix maven central url to get kuberentes json from

--- build.sh
+++ build.sh
@@ -4,35 +4,39 @@
 DEST="default/template"
 mkdir -p $DEST
 
-FABRIC8_IPAAS_VERSION=${FABRIC8_VERSION:-2.2.52}
-FABRIC8_DEVOPS_VERSION=${FABRIC8_VERSION:-2.2.52}
+FABRIC8_IPAAS_VERSION=${FABRIC8_IPAAS_VERSION:-2.2.59}
+FABRIC8_DEVOPS_VERSION=${FABRIC8_DEVOPS_VERSION:-2.2.61}
 
 
 echo "Installing templates from ipaas packages version #{FABRIC8_IPAAS_VERSION}"
 for app in amqbroker api-registry apiman apiman-gateway fabric8mq fabric8mq-consumer fabric8mq-producer qpid-dispatch; do
   echo "Installing ${app} template"
-  MVN_URL="http://central.maven.org/maven2/io/fabric8/ipaas/apps/${app}/${FABRIC8_VERSION}/${app}-${FABRIC8_VERSION}-kubernetes.json"
+  MVN_URL="http://central.maven.org/maven2/io/fabric8/ipaas/apps/${app}/${FABRIC8_IPAAS_VERSION}/${app}-${FABRIC8_IPAAS_VERSION}-kubernetes.json"
+  echo ${MVN_URL}
   curl -L -H "Accept: */*" ${MVN_URL} -o $DEST/${app}.json
 done
 
 echo "Installing templates from ipaas apps version #{FABRIC8_IPAAS_VERSION}"
 for app in apiman; do
   echo "Installing ${app} template"
-  MVN_URL="http://central.maven.org/maven2/io/fabric8/ipaas/packages/${app}/${FABRIC8_VERSION}/${app}-${FABRIC8_VERSION}-kubernetes.json"
+  MVN_URL="http://central.maven.org/maven2/io/fabric8/ipaas/packages/${app}/${FABRIC8_IPAAS_VERSION}/${app}-${FABRIC8_IPAAS_VERSION}-kubernetes.json"
+  echo ${MVN_URL}
   curl -L -H "Accept: */*" ${MVN_URL} -o $DEST/${app}.json
 done
 
 echo "Installing devops packages from version #{FABRIC8_DEVOPS_VERSION}"
 for app in cd-pipeline cdelivery cdelivery-core chat chat-irc chat-letschat chat-slack logging management metrics social; do
   echo "Installing ${app} app template"
-  MVN_QUICKSTART_URL="http://central.maven.org/maven2/io/fabric8/devops/packages/${app}/${FABRIC8_VERSION}/${app}-${FABRIC8_VERSION}-kubernetes.json"
-  curl -L -H "Accept: */*" ${MVN_QUICKSTART_URL} -o $DEST/${app}.json
+  MVN_URL="http://central.maven.org/maven2/io/fabric8/devops/packages/${app}/${FABRIC8_DEVOPS_VERSION}/${app}-${FABRIC8_DEVOPS_VERSION}-kubernetes.json"
+  echo ${MVN_URL}
+  curl -L -H "Accept: */*" ${MVN_URL} -o $DEST/${app}.json
 done
 echo "Installing devops apps from version #{FABRIC8_DEVOPS_VERSION}"
 for app in artifactory brackets chaos-monkey console-kubernetes gerrit gogs grafana2 hubot-irc hubot-letschat hubot-notifier hubot-slack image-linker jenkins keycloak kibana letschat nexus orion prometheus sonarqube taiga; do
   echo "Installing ${app} app template"
-  MVN_QUICKSTART_URL="http://central.maven.org/maven2/io/fabric8/devops/apps/${app}/${FABRIC8_VERSION}/${app}-${FABRIC8_VERSION}-kubernetes.json"
-  curl -L -H "Accept: */*" ${MVN_QUICKSTART_URL} -o $DEST/${app}.json
+  MVN_URL="http://central.maven.org/maven2/io/fabric8/devops/apps/${app}/${FABRIC8_DEVOPS_VERSION}/${app}-${FABRIC8_DEVOPS_VERSION}-kubernetes.json"
+  echo ${MVN_URL}
+  curl -L -H "Accept: */*" ${MVN_URL} -o $DEST/${app}.json
 done
 
 
