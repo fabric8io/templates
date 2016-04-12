dbb60b4746d8d7fc0df03778bce6ded1670208dd
rawlingsj <rawlingsj80@gmail.com>
2016-04-12 23:21:19 +0100

    add missing templates and remove invalid ones

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ FABRIC8_DEVOPS_VERSION=${FABRIC8_DEVOPS_VERSION:-2.2.157}
 
 
 echo "Installing templates from ipaas packages version #{FABRIC8_IPAAS_VERSION}"
-for app in apiman apiman-gateway elasticsearch-v1 example-message-consumer example-message-producer fabric8-camel fabric8-ipaas kafka message-broker message-gateway zookeeper ; do
+for app in apiman apiman-gateway elasticsearch-v1 example-message-consumer example-message-producer fabric8-camel kafka message-broker message-gateway zookeeper ; do
   echo "Installing ${app} template"
   MVN_URL="http://central.maven.org/maven2/io/fabric8/ipaas/apps/${app}/${FABRIC8_IPAAS_VERSION}/${app}-${FABRIC8_IPAAS_VERSION}-kubernetes.json"
   echo ${MVN_URL}
@@ -26,14 +26,14 @@ for app in apiman messaging ; do
 done
 
 echo "Installing devops packages from version #{FABRIC8_DEVOPS_VERSION}"
-for app in chat-irc chat-letschat chat-slack logging management metrics packages social; do
+for app in chat-irc chat-letschat chat-slack logging management metrics social; do
   echo "Installing ${app} app template"
   MVN_URL="http://central.maven.org/maven2/io/fabric8/devops/packages/${app}/${FABRIC8_DEVOPS_VERSION}/${app}-${FABRIC8_DEVOPS_VERSION}-kubernetes.json"
   echo ${MVN_URL}
   curl -L -H "Accept: */*" ${MVN_URL} -o $DEST/${app}.json
 done
 echo "Installing devops apps from version #{FABRIC8_DEVOPS_VERSION}"
-for app in artifactory brackets chaos-monkey collector-utils elasticsearch fabric8-devops fabric8-docker-registry fluentd gerrit git-collector gitlab gogs grafana2 hubot-irc hubot-letschat hubot-notifier hubot-slack image-linker jenkins keycloak kibana kiwiirc letschat nexus orion prometheus prometheus-blackbox-exporter sonarqube taiga; do
+for app in artifactory brackets chaos-monkey elasticsearch fabric8-docker-registry fluentd gerrit git-collector gitlab gogs grafana2 hubot-irc hubot-letschat hubot-notifier hubot-slack image-linker jenkins keycloak kibana kiwiirc letschat nexus orion prometheus prometheus-blackbox-exporter sonarqube taiga; do
   echo "Installing ${app} app template"
   MVN_URL="http://central.maven.org/maven2/io/fabric8/devops/apps/${app}/${FABRIC8_DEVOPS_VERSION}/${app}-${FABRIC8_DEVOPS_VERSION}-kubernetes.json"
   echo ${MVN_URL}
